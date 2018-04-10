96ef7af01a90749567202eb8467127d09158528d
Jinux <jinguol999@163.com>
2018-04-10 09:53:20 +0800

    feature: add warning dialog

---
+++ app/src/main/java/com/empty/jinux/simplediary/ui/dialog/WarningDialog.kt
@@ -0,0 +1,17 @@
+package com.empty.jinux.simplediary.ui.dialog
+
+import android.app.AlertDialog
+import android.content.Context
+import com.empty.jinux.simplediary.R
+
+/**
+ * Created by jinux on 18-4-10.
+ */
+fun Context.showWarningDialog(msg: String, next: () -> Unit) {
+    AlertDialog.Builder(this).apply {
+        setMessage(msg)
+        setIcon(R.drawable.ic_warning_black_24dp)
+        setPositiveButton(R.string.ok) { dialog, which -> next()}
+        setNegativeButton(R.string.cancel) { dialog, which -> }
+    }.create().show()
+}
