b549cd7d431f9b241b408ba042a052f4871c4b0e
Jinux <jinguol999@163.com>
2018-04-22 09:58:41 +0800

    change: change to BarChart for statistics view

--- app/src/main/java/com/empty/jinux/simplediary/ui/main/statistics/view/statistic/StatisticView.kt
+++ app/src/main/java/com/empty/jinux/simplediary/ui/main/statistics/view/statistic/StatisticView.kt
@@ -2,18 +2,23 @@ package com.empty.jinux.simplediary.ui.main.statistics.view.statistic
 
 import android.content.Context
 import android.graphics.Color
+import android.support.v4.content.ContextCompat
 import android.support.v7.widget.CardView
 import android.util.AttributeSet
 import android.view.LayoutInflater
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import com.empty.jinux.simplediary.R
 import com.empty.jinux.simplediary.data.Diary
 import com.empty.jinux.simplediary.util.dayTime
 import com.empty.jinux.simplediary.util.toCalendar
 import com.github.mikephil.charting.components.Legend
 import com.github.mikephil.charting.components.XAxis
-import com.github.mikephil.charting.data.*
+import com.github.mikephil.charting.data.BarData
+import com.github.mikephil.charting.data.BarDataSet
+import com.github.mikephil.charting.data.BarEntry
 import kotlinx.android.synthetic.main.layout_statistic_chart.view.*
-import kotlinx.android.synthetic.main.recycler_view_item_category.view.*
 import java.util.*
 
 class StatisticView
@@ -50,35 +55,42 @@ constructor(context: Context,
 
     fun setDiaries(data: List<Diary>) {
         val entries = data.groupBy { it.diaryContent.displayTime.dayTime() }.map {
-            Entry(it.key.toCalendar().get(Calendar.DAY_OF_YEAR).toFloat(),
-                    it.value.fold(0, {s, c -> s+c.diaryContent.content.length}).toFloat())
+            BarEntry((it.key.toCalendar().get(Calendar.DAY_OF_YEAR)).toFloat(),
+                    it.value.fold(0, { s, c -> s + c.diaryContent.content.length }).toFloat())
         }
-        val dataSet = LineDataSet(entries, "")
+        val dataSet = BarDataSet(entries, "")
         dataSet.color = Color.RED
         dataSet.form = Legend.LegendForm.NONE
-        dataSet.lineWidth = 5f
-        dataSet.mode = LineDataSet.Mode.CUBIC_BEZIER
+        dataSet.barBorderWidth = 3f
+//        dataSet.mode = LineDataSet.Mode.HORIZONTAL_BEZIER
         dataSet.isHighlightEnabled = false
-        dataSet.setValueTextColors(listOf(Color.BLACK))
-
-        statisticChat.data = LineData(dataSet).apply {
-            setDrawValues(false)
+        statisticChat.data = BarData(dataSet).apply {
+            setDrawValues(true)
+            setValueTextColor(ContextCompat.getColor(context, R.color.colorPrimary))
+            setValueTextSize(15f)
+            setValueFormatter { value, entry, dataSetIndex, viewPortHandler ->
+                value.toLong().toString()
+            }
         }
         statisticChat.invalidate()
         statisticChat.setVisibleXRangeMaximum(7f)
         statisticChat.setDrawGridBackground(false)
         statisticChat.setDrawBorders(false)
-        val xAxis = statisticChat.xAxis
-        xAxis.isEnabled = true
-        xAxis.isDrawAxisLineEnabled
-        xAxis.position = XAxis.XAxisPosition.BOTTOM
-        xAxis.setDrawGridLines(false)
-        xAxis.setDrawLabels(false)
+        statisticChat.xAxis.apply {
+            isEnabled = true
+            position = XAxis.XAxisPosition.BOTTOM
+            setDrawGridLines(false)
+            setDrawLabels(true)
+            axisLineColor = Color.BLACK
+        }
 
         statisticChat.axisRight.isEnabled = false
+
         statisticChat.axisLeft.apply {
             setDrawGridLines(false)
             setDrawLabels(false)
+            axisLineColor = Color.BLACK
+
         }
 
         statisticChat.description.isEnabled = false
