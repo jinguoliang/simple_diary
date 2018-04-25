d9a5cb3e9cbd315ff5d1711eb75cca2db54d2edd
Jinux <jinguol999@163.com>
2018-04-25 11:40:01 +0800

    change: verify the function of word count method

--- app/src/main/java/com/empty/jinux/simplediary/util/StringHelper.kt
+++ app/src/main/java/com/empty/jinux/simplediary/util/StringHelper.kt
@@ -1,18 +1,39 @@
 package com.empty.jinux.simplediary.util
 
 fun String.wordsCount(): Int {
-    val sentence = toCharArray()
     var inWord = false
     var wordCt = 0
-    for (c in sentence) {
+    for (c in this) {
         if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z') {
             if (!inWord) {
                 wordCt++
                 inWord = true
             }
+        } else if (isChineseByBlock(c)) {
+            if (!isChinesePunctuation(c)) {
+                wordCt++
+            }
+            inWord = false
         } else {
             inWord = false
         }
     }
     return wordCt
+}
+
+fun isChineseByBlock(c: Char): Boolean {
+    val ub = Character.UnicodeBlock.of(c)
+    return (ub === Character.UnicodeBlock.CJK_UNIFIED_IDEOGRAPHS
+            || ub === Character.UnicodeBlock.CJK_UNIFIED_IDEOGRAPHS_EXTENSION_A
+            || ub === Character.UnicodeBlock.CJK_UNIFIED_IDEOGRAPHS_EXTENSION_B
+            || ub === Character.UnicodeBlock.CJK_COMPATIBILITY_IDEOGRAPHS
+            || ub === Character.UnicodeBlock.CJK_COMPATIBILITY_IDEOGRAPHS_SUPPLEMENT)
+}
+
+fun isChinesePunctuation(c: Char): Boolean {
+    val ub = Character.UnicodeBlock.of(c)
+    return (ub === Character.UnicodeBlock.GENERAL_PUNCTUATION
+            || ub === Character.UnicodeBlock.CJK_SYMBOLS_AND_PUNCTUATION
+            || ub === Character.UnicodeBlock.HALFWIDTH_AND_FULLWIDTH_FORMS
+            || ub === Character.UnicodeBlock.CJK_COMPATIBILITY_FORMS)
 }
---
+++ app/src/test/java/com/empty/jinux/simplediary/util/StringHelperKtTest.kt
@@ -0,0 +1,40 @@
+package com.empty.jinux.simplediary.util
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class StringHelperKtTest {
+
+    @Test
+    fun wordsCount_english() {
+        val text = "hello world this is a test"
+        assertEquals(6, text.wordsCount())
+    }
+
+    @Test
+    fun wordsCount_chinese() {
+        val text = "这是个测试"
+        assertEquals(5, text.wordsCount())
+    }
+
+    @Test
+    fun wordsCount_chinese_english() {
+        val text = "这 h w"
+        assertEquals(3, text.wordsCount())
+    }
+
+    @Test
+    fun wordsCount_chinese_english1() {
+        val text = "h我 w"
+        assertEquals(3, text.wordsCount())
+    }
+
+    @Test
+    fun wordsCount_chinese_english_with_punctuation() {
+        val text = "我，w，hello。"
+        assertEquals(3, text.wordsCount())
+    }
+
+
+}
