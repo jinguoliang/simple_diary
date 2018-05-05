df9f4389ba47ca1971bc73104b94f6de05966c8a
Jinux <jinguol999@163.com>
2018-05-05 09:33:56 +0800

    change: remove the not-used firebase libraries

--- app/src/main/java/com/empty/jinux/simplediary/MApplication.kt
+++ app/src/main/java/com/empty/jinux/simplediary/MApplication.kt
@@ -1,18 +1,8 @@
 package com.empty.jinux.simplediary
 
-import android.net.Uri
-import com.empty.jinux.baselibaray.loge
-import com.empty.jinux.baselibaray.logi
-import com.empty.jinux.baselibaray.logw
 import com.empty.jinux.simplediary.di.DaggerMAppComponent
-import com.google.firebase.analytics.FirebaseAnalytics
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.storage.FirebaseStorage
-import com.google.firebase.storage.StorageReference
 import dagger.android.AndroidInjector
 import dagger.android.DaggerApplication
-import java.io.File
 
 
 class MApplication : DaggerApplication() {
