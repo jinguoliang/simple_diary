676580278baf145ae89d97844211adfd96cca30a
Jinux <jinguol999@163.com>
2018-05-10 11:56:30 +0800

    change: add actionbar in settings page

--- app/src/main/java/com/empty/jinux/simplediary/ui/settings/SettingsActivity.kt
+++ app/src/main/java/com/empty/jinux/simplediary/ui/settings/SettingsActivity.kt
@@ -2,14 +2,27 @@ package com.empty.jinux.simplediary.ui.settings
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.empty.jinux.simplediary.R
 import com.empty.jinux.simplediary.util.ActivityUtils
+import kotlinx.android.synthetic.main.activity_settings.*
 
 
 class SettingsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        ActivityUtils.addFragmentToActivity(supportFragmentManager, SettingsFragment(), android.R.id.content)
+        setContentView(R.layout.activity_settings)
+        setupToolbar()
+
+        ActivityUtils.addFragmentToActivity(supportFragmentManager, SettingsFragment(), R.id.content)
     }
+
+    private fun setupToolbar() {
+        setSupportActionBar(toolbar)
+        val ab = supportActionBar
+        ab!!.setDisplayHomeAsUpEnabled(true)
+        ab.setDisplayShowHomeEnabled(true)
+    }
+
 
 }
