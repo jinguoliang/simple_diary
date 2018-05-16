4ad09e235adb4937e652727a4dfbfe845f1d5875
Jinux <jinguol999@163.com>
2018-05-16 10:31:48 +0800

    change: add the extension method for adjust paragraph space

--- app/src/main/java/com/empty/jinux/simplediary/util/TextUtils.kt
+++ app/src/main/java/com/empty/jinux/simplediary/util/TextUtils.kt
@@ -1,6 +1,28 @@
 package com.empty.jinux.simplediary.util
 
+import android.text.SpannableStringBuilder
+import android.text.style.SuperscriptSpan
+import android.widget.EditText
+import kotlinx.android.synthetic.main.activity_demo.*
+
 fun String.getFirstLine(limitWidth: Int = 20): String {
     val firstLine = split("\n")[0]
     return firstLine.substring(0 until Math.min(firstLine.length, limitWidth))
+}
+
+fun EditText.adjustParagraphSpace(s: CharSequence?) {
+    if (s?.contains("\n") == true) {
+        val returnIndexs = s.mapIndexed { index, c -> if (c == '\n') index else -1 }.filterNot { it == -1 }
+        returnIndexs.forEachIndexed { index, i ->
+            val line = layout.getLineForOffset(i)
+            val lineStart = layout.getLineStart(line)
+            val spans = text.getSpans(lineStart, i, SuperscriptSpan::class.java)
+            if (spans.isEmpty()) {
+                text.setSpan(SuperscriptSpan(),
+                        lineStart,
+                        i,
+                        SpannableStringBuilder.SPAN_INCLUSIVE_INCLUSIVE)
+            }
+        }
+    }
 }
