f6b80db6e8a70a166824d009a382efd258e56199
Jinux <jinguol999@163.com>
2018-06-05 11:56:06 +0800

    bugfix: can't extract data from request result in release version because of proguard

--- app/src/main/java/com/empty/jinux/simplediary/weather/CurrentWeatherData.kt
+++ app/src/main/java/com/empty/jinux/simplediary/weather/CurrentWeatherData.kt
@@ -1,11 +1,13 @@
 package com.empty.jinux.simplediary.weather
 
+import com.google.gson.annotations.SerializedName
+
 /**
  * Created by jingu on 2018/2/25.
  */
-data class CurrentWeatherResult(val weather: List<Weather>)
+data class CurrentWeatherResult(@SerializedName("weather") val weather: List<Weather>)
 
-data class Weather(val id: Long,
-                   val main: String,
-                   val description: String,
-                   val icon: String)
+data class Weather(@SerializedName("id") val id: Long,
+                   @SerializedName("main") val main: String,
+                   @SerializedName("description") val description: String,
+                   @SerializedName("icon") val icon: String)
