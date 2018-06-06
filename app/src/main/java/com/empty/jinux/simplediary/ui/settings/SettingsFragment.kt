b7421ba085ee4fa2bfb0850800dce134f86f3b07
Jinux <jinguol999@163.com>
2018-06-06 19:54:17 +0800

    Revert "chagne: adjust the logic to save lock config"
    This reverts commit 2604fc49f9523ffbd25400c75efec8ab1effe876.

--- app/src/main/java/com/empty/jinux/simplediary/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/empty/jinux/simplediary/ui/settings/SettingsFragment.kt
@@ -26,9 +26,6 @@ class SettingsFragment : DaggerPreferenceFragment(),
                 mReporter.reportClick(key, checked.toString())
                 if (checked) {
                     // open lock
-                    PreferenceManager.getDefaultSharedPreferences(activity).edit {
-                        putBoolean("pref_app_lock_enable", false)
-                    }
                     showLockPasswordSetDialog()
                 }
             }
@@ -59,6 +56,10 @@ class SettingsFragment : DaggerPreferenceFragment(),
             dialog.cancel()
             mReporter.reportClick("password_set_dialog_cancel")
 
+            PreferenceManager.getDefaultSharedPreferences(activity).edit {
+                putBoolean("pref_app_lock_enable", false)
+            }
+
             val checkbox = preferenceManager.findPreference("pref_app_lock_enable") as CheckBoxPreference
             checkbox.isChecked = false
 
@@ -69,7 +70,6 @@ class SettingsFragment : DaggerPreferenceFragment(),
 
             PreferenceManager.getDefaultSharedPreferences(activity).edit {
                 putString("pref_app_lock_password", dialog.newPassword.text.toString())
-                putBoolean("pref_app_lock_enable", true)
             }
         }
         dialog.show()
