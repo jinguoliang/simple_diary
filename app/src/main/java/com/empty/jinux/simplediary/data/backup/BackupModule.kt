2275b4690ced7af8d0c81979514859435d4209d7
Jinux <jinguol999@163.com>
2018-06-14 12:55:33 +0800

    tmp

--- app/src/main/java/com/empty/jinux/simplediary/data/backup/BackupModule.kt
+++ app/src/main/java/com/empty/jinux/simplediary/data/backup/BackupModule.kt
@@ -1,14 +1,8 @@
 package com.empty.jinux.simplediary.data.backup
 
-import android.app.Activity
 import android.support.v4.app.Fragment
-import com.empty.jinux.simplediary.data.source.DiariesDataSource
-import com.empty.jinux.simplediary.data.source.DiariesRepository
-import com.empty.jinux.simplediary.di.Dang
 import com.empty.jinux.simplediary.di.Local
 import com.empty.jinux.simplediary.di.Remote
-import com.empty.jinux.simplediary.di.Repository
-import dagger.Binds
 import dagger.Module
 import dagger.Provides
 
@@ -17,26 +11,19 @@ import dagger.Provides
 abstract class BackupModule {
     @Module
     companion object {
+
         @JvmStatic
         @Provides
-        fun provideLocalBackup(context: Fragment): Backup {
+        @Local
+        fun provideRemoteBackup(context: Fragment): Backup {
             return LocalBackup(context)
         }
 
         @JvmStatic
         @Provides
         @Remote
-        fun provideRemoteBackup(context: Fragment): Backup {
-            return GoogleDriverBackup(context)
-        }
-
-        @JvmStatic
-        @Provides
-        @Local
         fun provideRemoteBackup1(context: Fragment): Backup {
             return GoogleDriverBackup(context)
         }
-
-
     }
 }
