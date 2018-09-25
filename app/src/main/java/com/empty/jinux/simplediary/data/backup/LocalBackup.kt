d8858169ad0fc32394012ee3a0cb3546ecdb25f6
Jinux <jinguol999@163.com>
2018-09-25 18:23:05 +0800

    change: now the data to backup is not only the database

--- app/src/main/java/com/empty/jinux/simplediary/data/backup/LocalBackup.kt
+++ app/src/main/java/com/empty/jinux/simplediary/data/backup/LocalBackup.kt
@@ -1,17 +1,14 @@
 package com.empty.jinux.simplediary.data.backup
 
-import android.content.Context
-import android.os.Environment
 import android.support.v4.app.Fragment
-import android.support.v7.app.AlertDialog
-import android.widget.ArrayAdapter
-import android.widget.Toast
-import androidx.core.widget.toast
-import com.empty.jinux.baselibaray.log.loge
-import com.empty.jinux.simplediary.R
 import com.empty.jinux.simplediary.data.source.local.room.DATABASE_NAME
 import com.google.common.io.Files
 import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.util.zip.ZipEntry
+import java.util.zip.ZipInputStream
+import java.util.zip.ZipOutputStream
 
 
 class LocalBackup(fragment: Fragment) : Backup() {
@@ -21,19 +18,50 @@ class LocalBackup(fragment: Fragment) : Backup() {
 
     override fun backupDb(outPath: String) {
         val databaseFilePath = context.getDatabasePath(DATABASE_NAME).toString();
-        Files.copy(File(databaseFilePath), File(outPath))
+        val zipOut = ZipOutputStream(FileOutputStream(outPath))
+        zipOut.putNextEntry(ZipEntry(databaseFilePath))
+        Files.copy(File(databaseFilePath), zipOut)
+
+        val imagesDir = "${context!!.filesDir}/images".run { File(this) }
+        if (imagesDir.isDirectory) {
+            for (x in imagesDir.listFiles()) {
+                zipOut.putNextEntry(ZipEntry(x.absolutePath))
+                Files.copy(x, zipOut)
+                zipOut.closeEntry()
+
+            }
+        }
+        zipOut.flush()
+        zipOut.close()
     }
 
     override fun importDb(inPath: String) {
         val databaseFilePath = context.getDatabasePath(DATABASE_NAME).toString();
 
-        Files.copy(File(inPath), File(databaseFilePath))
+        val zipIn = ZipInputStream(FileInputStream(inPath))
+        var entry: ZipEntry? = zipIn.nextEntry
+        while (null != entry) {
+            copy(zipIn, entry.name)
+            zipIn.closeEntry()
+            entry = zipIn.nextEntry
+        }
+        zipIn.close()
 
         val databaseShm = "$databaseFilePath-shm"
         val databaseWal = "$databaseFilePath-wal"
         File(databaseShm).delete()
         File(databaseWal).delete()
+    }
 
+    private fun copy(zipIn: ZipInputStream, name: String) {
+        val buffer = ByteArray(64)
+        var n = zipIn.read(buffer)
+        val fileOutputStream = FileOutputStream(name)
+        while (n > 0) {
+            fileOutputStream.write(buffer, 0, n)
+            n = zipIn.read(buffer)
+        }
+        fileOutputStream.close()
     }
 
 }
