612c7b7655b7bd9fe5c984c575496cf33849210f
Jinux <jinguol999@163.com>
2018-11-23 01:25:20 +0800

    showing Progress Dialog need the context is Activity

--- baselibrary/src/main/java/com/empty/jinux/baselibaray/view/loading/KLoadingDialog.kt
+++ baselibrary/src/main/java/com/empty/jinux/baselibaray/view/loading/KLoadingDialog.kt
@@ -2,13 +2,14 @@
 
 package com.empty.jinux.baselibaray.view.loading
 
+import android.app.Activity
 import android.app.ProgressDialog
 import android.content.Context
 import com.empty.jinux.baselibaray.R
 import com.empty.jinux.baselibaray.thread.ThreadPools
 
 
-fun Context.doTaskWithLoadingDialog(msg: String, delay: Long = 1000, task: () -> Unit) {
+fun Activity.doTaskWithLoadingDialog(msg: String, delay: Long = 1000, task: () -> Unit) {
     val d = ProgressDialog.show(this, "", msg)
     ThreadPools.postOnQuene {
         task.invoke()
